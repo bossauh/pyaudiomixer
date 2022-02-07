e2058160a5c43a48eb62b8136ebb676a7b0ff559
matt <philmattdev@gmail.com>
2022-02-08 01:28:54 +0800

    feat: target version in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 
 install_requires = [
-    "sounddevice",
-    "soundfile",
-    "librosa",
-    "soxr",
-    "numpy",
-    "ffmpy"
+    "sounddevice==0.4.4",
+    "soundfile==0.10.3.post1",
+    "librosa==0.8.1",
+    "sox==1.4.1",
+    "numpy==1.20.3",
+    "ffmpy==0.3.0"
 ]
