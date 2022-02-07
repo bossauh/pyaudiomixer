f4f5c13b53fc068a6f54bfc255560c2d005468bb
matt <philmattdev@gmail.com>
2022-02-08 01:57:59 +0800

    chore: add setup function

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+from distutils.core import setup
+
 
 install_requires = [
     "sounddevice==0.4.4",
@@ -5,5 +7,30 @@ install_requires = [
     "librosa==0.8.1",
     "sox==1.4.1",
     "numpy",
-    "ffmpy==0.3.0"
+    "ffmpy==0.3.0",
+    "maglevapi"
 ]
+
+
+setup(
+    name="pyaudiomixer",
+    packages=["pyaudio_mixer"],
+    version="0.1",
+    license="MIT",
+    description="Audio mixing simplified.",
+    author="Philippe Mathew",
+    author_email="philmattdev@gmail.com",
+    url="",
+    download_url="",
+    keywords=["audio", "mixer", "mixing", "tool"],
+    install_requires=install_requires,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ]
+)
+
