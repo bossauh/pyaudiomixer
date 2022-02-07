588980ae5e5f897e4eb435b665e54f21150481e8
matt <philmattdev@gmail.com>
2022-02-08 03:55:46 +0800

    test: add mixer test

--- run_tests.py
+++ run_tests.py
@@ -1,11 +1,13 @@
 import asyncio
 from tests.output import TestOutput
 from tests.input import TestInput
+from tests.mixer import TestMixer
 
 
 async def main() -> None:
     await TestInput().run()
     await TestOutput().run()
+    await TestMixer().run()
 
 
 if __name__ == "__main__":
